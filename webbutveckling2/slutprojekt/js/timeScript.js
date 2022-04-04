7a999886dd47abc63b6196842b78dc254c3f1659
villegust <ville.gust@gmail.com>
2022-04-04 10:54:09 +0200

    -

--- webbutveckling2/slutprojekt/js/timeScript.js
+++ webbutveckling2/slutprojekt/js/timeScript.js
@@ -1,5 +1,13 @@
 //tids script till reviews
-let currentDate = new Date();
-let time = currentDate.getHours() + ":" + currentDate.getMinutes()
-document.write(time)
+const current = new Date();
 
+const time = current.toLocaleTimeString("en-US", {
+  hour: "2-digit",
+  minute: "2-digit",
+  hour12: false
+});
+
+document.getElementById("tid1").innerText += time
+document.getElementById("tid2").innerText += time
+document.getElementById("tid3").innerText += time
+document.getElementById("tid4").innerText += time
